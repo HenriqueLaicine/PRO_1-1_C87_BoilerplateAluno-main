23aa3fa8b473d465ec0ab3a43240fb7931e6213f
Henrique Laicine <hlaicine@gmail.com>
2023-08-31 15:52:44 -0300

    Aula88

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAymStGRuzgNzpwU7HvPZjGllBufdegAKE",
+    authDomain: "storytelling-d2cd4.firebaseapp.com",
+    databaseURL: "https://storytelling-d2cd4-default-rtdb.firebaseio.com",
+    projectId: "storytelling-d2cd4",
+    storageBucket: "storytelling-d2cd4.appspot.com",
+    messagingSenderId: "745435081888",
+    appId: "1:745435081888:web:1d059fc9fc686bae5dedc5"
+  };
